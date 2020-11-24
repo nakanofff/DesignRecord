eeacd18eaa95794b8bf00861ebc6b1f0062cfe20
nikuudon0027 <53633479+nikuudon0027@users.noreply.github.com>
2020-11-24 18:44:14 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#menu_button').click(function(){
+    $('#menu').addClass('close_menu');
+  });
+  $('#close_button').click(function(){
+    $('#menu').removeClass('close_menu');
+  });
+});
